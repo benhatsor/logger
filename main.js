4d5a814781548e8cf7f06b7ef2cebcebd26255c2
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-01-25 11:35:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 
+const output = document.querySelector('.output');
+
+function logCallback(type, data) {
+  
+  // add spaces between adjacent arguments
+  data = data.join(' ');
+  
+  output.innerHTML += '<div class="item '+ type +'"><a class="type">[' + type + ']</a> <a class="data">' + data + '</a></div>';
+  
+}
+
 logger.init(logCallback, window);
 
 console.log('hello');
