9120f22a8d6d0b0bb0b78b9f063adde9ae38f9c2
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-01-25 11:35:08 +0200

    Update logger.js

--- logger.js
+++ logger.js
@@ -309,20 +309,3 @@ const logger = {
     
 }
 
-
-
-
-
-
-
-const output = document.querySelector('.output');
-
-function logCallback(type, data) {
-  
-  // add spaces between adjacent arguments
-  data = data.join(' ');
-  
-  output.innerHTML += '<div class="item '+ type +'"><a class="type">[' + type + ']</a> <a class="data">' + data + '</a></div>';
-  
-}
-
