d1c558d91b4af76235a1c08491d8c9f21c778f46
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-01-26 11:33:37 +0200

    Update logger.js

--- logger.js
+++ logger.js
@@ -233,7 +233,7 @@ let logger = {
           if (buffer.length / 2 < 1) {
           
             // return function
-            return o.toString()//.split('\n    ').join('\n' + buffer);
+            return o.toString();//.split('\n    ').join('\n' + buffer);
         
           } else {
             
@@ -248,7 +248,7 @@ let logger = {
             } else {
               
               // return function name
-              return o.name;
+              return o.name + '()';
               
             }
             
