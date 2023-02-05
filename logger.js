ff180beddd2ac1773d2b8df5a8d14166ea952fa6
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-02-05 19:58:47 +0200

    Update logger.js

--- logger.js
+++ logger.js
@@ -374,7 +374,7 @@ let logger = {
           names.sort(sortci);
           for (i = 0; i < names.length; i++) {
             try {
-              parts.push(newBuffer + names[i] + ': ' + stringify(o[names[i]], visited, newBuffer));
+              parts.push(newBuffer + names[i] + ': ' + stringify(o[names[i]], visited, buffer));
             } catch (e) {}
           }
 
